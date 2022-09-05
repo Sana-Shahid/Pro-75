a127482085c8a2cfccbda0394263c2574e9c550e
Sana-Shahid <77516425+Sana-Shahid@users.noreply.github.com>
2022-09-05 09:32:58 -0400

    Add files via upload

--- config.js
+++ config.js
@@ -1,14 +1,13 @@
 import firebase from "firebase";
 require("@firebase/firestore");
 
-var firebaseConfig = {
-  apiKey: "AIzaSyCZrG0BF6LrnRpWZjsPQxNGyMCLByo8HrM",
-  authDomain: "e-ride-549e4.firebaseapp.com",
-  projectId: "e-ride-549e4",
-  storageBucket: "e-ride-549e4.appspot.com",
-  messagingSenderId: "588298110700",
-  appId: "1:588298110700:web:34e3df331c6b1fc95047c9",
-  measurementId: "G-G3K7BWL5LH"
+const firebaseConfig = {
+  apiKey: "AIzaSyDqJ_jv9ujwYjJBwn2Zj2vxpiNduEGJDdg",
+  authDomain: "e-ride-stage-4-7c620.firebaseapp.com",
+  projectId: "e-ride-stage-4-7c620",
+  storageBucket: "e-ride-stage-4-7c620.appspot.com",
+  messagingSenderId: "357197295048",
+  appId: "1:357197295048:web:f61cdbe7913d6c0c8fa4fd"
 };
 
 firebase.initializeApp(firebaseConfig);
